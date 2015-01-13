14dc35e58612f651798ef73ed82df75557185d3b
alex <alex@nabu.be>
2015-01-13 13:40:53 +0100

    Added factory for channel orchestrator

---
+++ src/main/java/be/nabu/libs/channels/ChannelOrchestratorFactory.java
@@ -0,0 +1,36 @@
+package be.nabu.libs.channels;
+
+import be.nabu.libs.channels.api.ChannelOrchestrator;
+
+/**
+ * This factory allows for centralized configuration but does not currently allow service-like detection
+ */
+public class ChannelOrchestratorFactory {
+
+	private static ChannelOrchestratorFactory instance;
+	
+	public static ChannelOrchestratorFactory getInstance() {
+		if (instance == null) {
+			synchronized(ChannelOrchestratorFactory.class) {
+				if (instance == null) {
+					instance = new ChannelOrchestratorFactory();
+				}
+			}
+		}
+		return instance;
+	}
+	
+	private ChannelOrchestrator channelOrchestrator;
+	
+	public void setChannelManager(ChannelOrchestrator channelOrchestrator) {
+		this.channelOrchestrator = channelOrchestrator;
+	}
+	
+	public void unsetChannelManager(ChannelOrchestrator channelOrchestrator) {
+		this.channelOrchestrator = null;
+	}
+
+	public ChannelOrchestrator getChannelOrchestrator() {
+		return channelOrchestrator;
+	}
+}
